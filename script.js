08cfd5e71bd649858dbb7abe670daf2a8e078839
izam-dev <rayzox523@gmail.com>
2026-06-05 21:12:14 +0700

    optimize

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const btnorder = document.getElementById("order")
-const btnproduct = document.getElementById("product")
-btnorder.addEventListener("click",() => {
-    const masage = ` halo saya mau beli 
+
+const btnproduct = document.querySelectorAll(".btn-order")
+
+btnproduct.forEach((button) =>{
+    button.addEventListener("click", () => {
+        const masage = ` halo saya mau beli 
     Nama :
     jumlah :
     Tanggal penjemputan :`;
-    const url = `https://wa.me/62882003067694?text=${encodeURIComponent(masage)}`;
-    window.open(url,"_blank");
-});
-btnproduct.addEventListener("click", () => {
-    const masage = ` halo saya mau beli 
-    Nama :
-    jumlah :
-    Tanggal penjemputan :`;
-    const url = `https://wa.me/62882003067694?text=${encodeURIComponent(masage)}`;
-    window.open(url, "_blank");
+        const url = `https://wa.me/62882003067694?text=${encodeURIComponent(masage)}`;
+        window.open(url, "_blank");
+    });
 });
